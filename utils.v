7e5577fc16dbf8805fe85d5efc1453f8c7de1896
Naoki MATSUMOTO <naoki@pibvt.net>
2022-01-29 17:12:50 +0900

    Everything restarts from here

--- utils.v
+++ utils.v
@@ -1,36 +1,31 @@
 module main
 
-fn bytes_to_u16(buf []byte) ?u16 {
+fn le_bytes_to_u16(buf []byte) ?u16 {
     assert buf.len == 2
-    return buf[0] | buf[1] << 8
+    return u16(buf[0]) | u16(buf[1]) << 8
 }
 
-fn bytes_to_int(buf []byte) ?int {
+fn le_bytes_to_u32(buf []byte) ?u32 {
     assert buf.len == 4
-    return buf[0] | buf[1] << 8 | buf[2] << 16 | buf[3] << 24
+    return u32(buf[0]) | u32(buf[1]) << 8 | u32(buf[2]) << 16 | u32(buf[3]) << 24
 }
 
-fn bytes_to_u32(buf []byte) ?u32 {
-    assert buf.len == 4
-    return buf[0] | buf[1] << 8 | buf[2] << 16 | buf[3] << 24
-}
-
-fn bytes_to_u64(buf []byte) ?u64 {
+fn le_bytes_to_u64(buf []byte) ?u64 {
     assert buf.len == 8
     mut tmp := u64(0)
-    tmp |= buf[0] | buf[1] << 8 | buf[2] << 16 | buf[3] << 24
-    tmp |= buf[4] << 32 | buf[5] << 40 | buf[6] << 48 | buf[7] << 56
+    tmp |= u64(buf[0]) | u64(buf[1]) << 8 | u64(buf[2]) << 16 | u64(buf[3]) << 24
+    tmp |= u64(buf[4]) << 32 | u64(buf[5]) << 40 | u64(buf[6]) << 48 | u64(buf[7]) << 56
     return tmp
 }
 
-fn u16_to_bytes(val u16) []byte {
+fn le_u16_to_bytes(val u16) []byte {
     mut buf := []byte{len:2}
     buf[0] = byte(val)
     buf[1] = byte(val >> 8)
     return buf
 }
 
-fn int_to_bytes(val int) []byte {
+fn le_u32_to_bytes(val u32) []byte {
     mut buf := []byte{len:4}
     buf[0] = byte(val)
     buf[1] = byte(val >> 8)
@@ -39,29 +34,7 @@ fn int_to_bytes(val int) []byte {
     return buf
 }
 
-fn u32_to_bytes(val u32) []byte {
-    mut buf := []byte{len:4}
-    buf[0] = byte(val)
-    buf[1] = byte(val >> 8)
-    buf[2] = byte(val >> 16)
-    buf[3] = byte(val >> 24)
-    return buf
-}
-
-fn u64_to_bytes(val u64) []byte {
-    mut buf := []byte{len:8}
-    buf[0] = byte(val)
-    buf[1] = byte(val >> 8)
-    buf[2] = byte(val >> 16)
-    buf[3] = byte(val >> 24)
-    buf[4] = byte(val >> 32)
-    buf[5] = byte(val >> 40)
-    buf[6] = byte(val >> 48)
-    buf[7] = byte(val >> 56)
-    return buf
-}
-
-fn i64_to_bytes(val i64) []byte {
+fn le_u64_to_bytes(val u64) []byte {
     mut buf := []byte{len:8}
     buf[0] = byte(val)
     buf[1] = byte(val >> 8)
@@ -95,25 +68,20 @@ fn be_u32_to_bytes(val u32) []byte {
 fn be_bytes_to_u32(buf []byte) ?u32 {
     assert buf.len == 4
     mut val := u32(0)
-    val |= buf[0] << 24
-    val |= buf[1] << 16
-    val |= buf[2] << 8
-    val |= buf[3] << 0
+    val |= u32(buf[0]) << 24
+    val |= u32(buf[1]) << 16
+    val |= u32(buf[2]) << 8
+    val |= u32(buf[3]) << 0
     return val
-}
-
-fn be16(buf []byte) u16 {
-    assert buf.len == 2
-    return buf[0] << 8 | buf[1]
 }
 
 fn calc_chksum(buf []byte) u16 {
     mut chksum := u32(0)
     for i := 0; i < buf.len; i += 2 {
         if i + 1 >= buf.len {
-            chksum += buf[i] << 8
+            chksum += u16(buf[i]) << 8
         } else {
-            chksum += (buf[i] << 8) | buf[i+1]
+            chksum += (u16(buf[i]) << 8) | buf[i+1]
         }
 
         if chksum > 0xFFFF {
